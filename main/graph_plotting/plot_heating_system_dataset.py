bc8e86bb750078206d30927faa5cf7a373f1ebfa
Georgij <netbay.georgij@gmail.com>
2021-03-08 01:21:40 +0500

    ~~~

--- main/graph_plotting/plot_heating_system_dataset.py
+++ main/graph_plotting/plot_heating_system_dataset.py
@@ -4,23 +4,23 @@ import matplotlib.pyplot as plt
 from dateutil.tz import gettz
 from pandas.plotting import register_matplotlib_converters
 
-from main import config
-from heating_system_utils.constants import column_names
-from heating_system.preprocess_utils import average_values, filter_by_timestamp_closed
+from heating_system.preprocess_utils import filter_by_timestamp_closed
 from heating_system_utils.boiler_dataset_io import load_dataset
+from heating_system_utils.constants import column_names
+from main import config
 
 if __name__ == '__main__':
     start_datetime = datetime.datetime(2018, 12, 1, 0, 0, 0, tzinfo=gettz(config.TIMEZONE))
     end_datetime = datetime.datetime(2019, 3, 1, 0, 0, 0, tzinfo=gettz(config.TIMEZONE))
-    # dataset_path = config.BOILER_PREPROCESSED_HEATING_CIRCUIT_DATASET_PATH
-    dataset_path = f"{config.HOMES_PREPROCESSED_HEATING_CIRCUIT_DATASETS_DIR}\\engelsa_37.csv.pickle"
+    dataset_path = config.BOILER_PREPROCESSED_HEATING_CIRCUIT_DATASET_PATH
+    # dataset_path = f"{config.HOMES_PREPROCESSED_HEATING_CIRCUIT_DATASETS_DIR}\\engelsa_37.csv.pickle"
 
     register_matplotlib_converters()
     ax = plt.axes()
 
     columns_to_plot = (
         column_names.FORWARD_PIPE_COOLANT_TEMP,
-        # column_names.BACKWARD_PIPE_COOLANT_TEMP,
+        column_names.BACKWARD_PIPE_COOLANT_TEMP,
         # column_names.FORWARD_PIPE_COOLANT_VOLUME,
         # column_names.BACKWARD_PIPE_COOLANT_VOLUME,
         # column_names.FORWARD_PIPE_COOLANT_PRESSURE,
